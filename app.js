e79307532c3dd1302abd38462a68fb8798c18854
AriellaRogueN <arrodriguezn@gmail.com>
2017-11-07 20:17:45 -0300

    agregandomodificaciones

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
+function boardActive(){
+/*identifico el campo oculto*/
+    var contenedor = document.getElementById("form")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    /*lo hago visible a traves de display block*/
+    contenedor.style.display="block";
+    
 }
+
