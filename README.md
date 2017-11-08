27f93137ed3ca528c36b4823ef6a6534be97d20d
AriellaRogueN <arrodriguezn@gmail.com>
2017-11-07 23:54:46 -0300

    version 0.0.1

---
+++ README.md
@@ -0,0 +1,19 @@
+# Agregando Funcionalidad a Página web simil Trello.
+
+- - -
+### Reto de Código que agrega contenido dinámico a una página web, combinando HTML,CSS y JS. Para la realización de este reto se siguen  ejemplos otorgados por los profesores y autoaprendizaje.
+
+#### Flujo de trabajo.
+##### El flujo de trabajo se basa en la generación de distintas versiones a medida que se van realizando avances en la funcionalidad, de acuerdo a los retos establecidos.
+
+
+
+
+
+
+
+
+- - -
+
+
+Pagina Web creada por Ariella Rodríguez Núñez.
--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-function boardActive(){
-/*identifico el campo oculto*/
+function boardActive() {
+    /*identifico el campo oculto*/
     var contenedor = document.getElementById("form")
 
     /*lo hago visible a traves de display block*/
-    contenedor.style.display="block";
-    
+    contenedor.style.display = "block";
 }
 
+function boardInactive() {
+    /*identifico el campo (input) que quiero ocultar*/
+    var inactive = document.getElementById("addText");
+
+    /*Lo oculto a traves de display none*/
+    inactive.style.display = "none";
+
+}
